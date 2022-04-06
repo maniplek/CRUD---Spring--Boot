e22fcf5f4408372dd5aeeaef9efb1a18ecbfdc80
Maniple <maniplek81@gmail.com>
2022-04-06 10:56:45 +0200

    CRUD SPRING

---
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Department;
+import com.example.demo.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/departments")
+    public Department saveDepartment(@Valid @RequestBody Department department){
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departmentList")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        departmentService.deleteDepartmentById(departmentId);
+        return "Department Deleted successfully!";
+    }
+
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId,
+                                       @RequestBody Department department){
+        return departmentService.updateDepartment(departmentId, department);
+    }
+
+    @GetMapping("/departments/name/{name}")
+    public Department fetchDepartmentByName(@PathVariable("name") String departmentName){
+        return departmentService.fetchDepartmentByName(departmentName);
+    }
+
+}
